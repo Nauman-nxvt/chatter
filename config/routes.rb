d2cd1272648fabb4d718013bc79d28956734337a
Nauman Tahir <nomibzu@gmail.com>
2017-05-04 19:09:32 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :messages
   resources :users, only: [ :create]
   root to: 'users#login'
-  #get '/chat' => 'chat#index', as: 'chat'
   get '/log_out' => 'users#log_out', as: 'logout'
 end
